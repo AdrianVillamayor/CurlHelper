815cf55061a3d93062e285abbde250438c824f11
Adrián Villamayor <adrian.villamayor@gmail.com>
2020-06-25 10:13:38 +0200

    Add files via upload

---
+++ CurlHelper.php
@@ -0,0 +1,224 @@
+<?php
+
+/**
+ *
+ * This class has all the necessary code for making API calls thru curl library
+ * @category   Helper
+ * @author     Original Author <adrian@socialdiabetes.com>
+ *
+ */
+
+class CurlHelper
+{
+    /**
+     * @var string
+     */
+    public $user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36';
+
+    /**
+     * @var int
+     */
+    public $timeout = 30;
+
+    /**
+     * @var resource
+     */
+    protected $ch;
+
+    /**
+     * @var null|string
+     */
+    protected $url;
+
+    /**
+     * @var array
+     */
+    protected $get_data = [];
+
+    /**
+     * @var array
+     */
+    protected $post_data = [];
+
+    /**
+     * @var null|string
+     */
+    protected $post_raw;
+
+    /**
+     * @var array
+     */
+    protected $cookies = [];
+
+    /**
+     * @var array
+     */
+    protected $headers = [];
+
+    /**
+     * @var array
+     */
+    protected $xpath = [];
+
+    /**
+     * @var array
+     */
+    protected $files = [];
+
+    /**
+     * @var array
+     */
+    protected $response = [];
+
+    /**
+     * @var string
+     */
+    public $http_code;
+
+    /**
+     * @var mixed
+     */
+    public $debug;
+
+    const MIME_X_WWW_FORM   = 'application/x-www-form-urlencoded';
+    const MIME_FORM_DATA    = 'multipart/form-data';
+    const MIME_JSON         = 'application/json';
+
+    /**
+     * This method will perform an action/method thru HTTP/API calls
+     *
+     * @param string $method POST, PUT, GET etc
+     * @param string $url
+     * @param string $mime Type of data (form, multipart, json)
+     * @param array $data Array
+     * @param bool $debug Default False
+     * 
+
+     * @return void
+     */
+
+    public function perform_http_request($method, $url, $mime, $data, $debug = false): void
+    {
+        $this->ch  = curl_init();
+        $this->url = $url;
+
+        switch ($mime) {
+            case 'form':
+                $content_type =  $this->MIME_X_WWW_FORM;
+                break;
+
+            case 'multipart':
+                $content_type =  $this->MIME_FORM_DATA;
+                break;
+
+            case 'json':
+                $content_type =  $this->MIME_JSON;
+                $data = json_encode($data);
+                break;
+
+            default:
+                $content_type =  $this->MIME_JSON;
+                break;
+        }
+
+        switch ($method) {
+            case "POST":
+                curl_setopt($this->ch, CURLOPT_POST, 1);
+
+                if (!empty($data)) {
+                    curl_setopt($this->ch, CURLOPT_POSTFIELDS, $data);
+                }
+                break;
+
+            case "PUT":
+                curl_setopt($this->ch, CURLOPT_PUT, 1);
+                break;
+
+            default:
+                if (!empty($data)) {
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+                }
+                break;
+        }
+
+        curl_setopt($this->ch, CURLOPT_URL, $this->url);
+
+        curl_setopt($this->ch, CURLOPT_USERAGENT, $this->user_agent);
+
+        curl_setopt($this->ch, CURLOPT_HTTPHEADER, array(
+            "Content-Type: {$content_type}; charset=utf-8",
+        ));
+
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $this->response = curl_exec($this->ch);
+
+        $this->http_code = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+
+        if ($debug) {
+            $this->debug = curl_getinfo($this->ch);
+        }
+
+        curl_close($this->ch);
+    }
+
+    public function http_code(): string
+    {
+        return $this->http_code;
+    }
+
+    public function debug(): ?array
+    {
+        return $this->debug;
+    }
+
+    public function response(): ?array
+    {
+        $response = json_decode($this->response);
+
+        if (json_last_error() == JSON_ERROR_NONE) {
+            return (array) $response;
+        }
+
+        if($this->response == "" || empty($this->response)){
+            return null;
+        }
+
+        return $this->response;
+    }
+
+    public function parseCode(): array
+    {
+        $error = true;
+
+        switch ($this->http_code) {
+            case '200':
+            case '201':
+                $msg = _("Success");
+                $error = false;
+                break;
+
+            case '409':
+                $msg = _("This email is already registered.");
+                break;
+
+            case '403':
+                $msg = _("Forbidden, denied access to the requested action.");
+                break;
+           
+            case '404':
+                $msg = _("Nothing has been found.");
+                break;
+
+            case '401':
+                $msg = _("Unauthorized");
+                break;
+
+            case '405':
+                $msg = _("Method Not Allowed");
+                break;
+        }
+
+        return array($error, $msg);
+    }
+}
