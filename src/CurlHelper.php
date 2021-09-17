bf7b75a6d288259c3e7864d73a2add8ad605edd4
Adrii <adrian.villamayor@gmail.com>
2021-09-17 09:15:02 +0200

    Update

--- src/CurlHelper.php
+++ src/CurlHelper.php
@@ -371,30 +371,126 @@ class CurlHelper
         $error = true;
 
         switch ($this->http_code) {
-            case '200':
-            case '201':
-                $msg    = "Success";
+            case "100":
+                $msg = 'Continue';
+                break;
+            case "101":
+                $msg = 'Switching Protocols';
+                break;
+            case "200":
+                $msg = 'OK';
                 $error  = false;
                 break;
-
-            case '409':
-                $msg    = "This email is already registered.";
+            case "201":
+                $msg = 'Created';
+                $error  = false;
                 break;
-
-            case '403':
-                $msg    = "Forbidden, denied access to the requested action.";
+            case "202":
+                $msg = 'Accepted';
+                $error  = false;
                 break;
-
-            case '404':
-                $msg    = "Nothing has been found.";
+            case "203":
+                $msg = 'Non-Authoritative Information';
+                $error  = false;
                 break;
-
-            case '401':
-                $msg    = "Unauthorized";
+            case "204":
+                $msg = 'No Content';
+                $error  = false;
                 break;
-
-            case '405':
-                $msg    = "Method Not Allowed";
+            case "205":
+                $msg = 'Reset Content';
+                $error  = false;
+                break;
+            case "206":
+                $msg = 'Partial Content';
+                $error  = false;
+                break;
+            case "300":
+                $msg = 'Multiple Choices';
+                break;
+            case "301":
+                $msg = 'Moved Permanently';
+                break;
+            case "302":
+                $msg = 'Moved Temporarily';
+                break;
+            case "303":
+                $msg = 'See Other';
+                break;
+            case "304":
+                $msg = 'Not Modified';
+                break;
+            case "305":
+                $msg = 'Use Proxy';
+                break;
+            case "400":
+                $msg = 'Bad Request';
+                break;
+            case "401":
+                $msg = 'Unauthorized';
+                break;
+            case "402":
+                $msg = 'Payment Required';
+                break;
+            case "403":
+                $msg = 'Forbidden';
+                break;
+            case "404":
+                $msg = 'Not Found';
+                break;
+            case "405":
+                $msg = 'Method Not Allowed';
+                break;
+            case "406":
+                $msg = 'Not Acceptable';
+                break;
+            case "407":
+                $msg = 'Proxy Authentication Required';
+                break;
+            case "408":
+                $msg = 'Request Time-out';
+                break;
+            case "409":
+                $msg = 'Conflict';
+                break;
+            case "410":
+                $msg = 'Gone';
+                break;
+            case "411":
+                $msg = 'Length Required';
+                break;
+            case "412":
+                $msg = 'Precondition Failed';
+                break;
+            case "413":
+                $msg = 'Request Entity Too Large';
+                break;
+            case "414":
+                $msg = 'Request-URI Too Large';
+                break;
+            case "415":
+                $msg = 'Unsupported Media Type';
+                break;
+            case "500":
+                $msg = 'Internal Server Error';
+                break;
+            case "501":
+                $msg = 'Not Implemented';
+                break;
+            case "502":
+                $msg = 'Bad Gateway';
+                break;
+            case "503":
+                $msg = 'Service Unavailable';
+                break;
+            case "504":
+                $msg = 'Gateway Time-out';
+                break;
+            case "505":
+                $msg = 'HTTP Version not supported';
+                break;
+            default:
+                $msg = "Unknown http status code " . $this->http_code;
                 break;
         }
 
