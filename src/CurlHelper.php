14df64d48e62fb5ab82adb6c5c3ebe24387c0a59
Adrii <adrian.villamayor@gmail.com>
2022-01-13 11:25:51 +0100

    Update

--- src/CurlHelper.php
+++ src/CurlHelper.php
@@ -18,8 +18,10 @@ class CurlHelper
     public string $user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36';
 
     public int $timeout = 30;
-
-    protected \CurlHandle $ch;
+    /**
+     * @var \CurlHandle
+     */
+    protected  $ch;
 
     protected ?string $url;
 
