25cdf9d7bfe16fd34717a2ba91ae31a0f96640da
Adrii <adrian.villamayor@gmail.com>
2022-01-13 11:34:43 +0100

    asd

--- src/CurlHelper.php
+++ src/CurlHelper.php
@@ -18,6 +18,7 @@ class CurlHelper
     public string $user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36';
 
     public int $timeout = 30;
+   
     /**
      * @var \CurlHandle
      */
@@ -190,11 +191,9 @@ class CurlHelper
         if (!empty($this->post_data)) {
             curl_setopt($this->ch, CURLOPT_POST, TRUE);
 
-            if ($this->mime === $this->mime_type['MIME_JSON']) {
-                $this->post_data = json_encode($this->post_data);
-            }
+            $post_data = ($this->mime === $this->mime_type['MIME_JSON']) ? json_encode($this->post_data) : $this->post_data;
 
-            curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->post_data);
+            curl_setopt($this->ch, CURLOPT_POSTFIELDS, $post_data);
             $this->headers['Content-Type'] = $this->parseMimeType();
         }
 
